198f9657f1095689753e5b80c7aebb7250a6f1a6
João Victor Dias de Moraes <54956887+jvxdev@users.noreply.github.com>
2021-08-14 20:23:53 -0300

    Início de implementação. Utilização do styled-components
    Criação do projeto.

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,2 +1,13 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Title = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    color: green;
+`;

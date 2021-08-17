b08bf7005dfc290a02b7cca87023b91629a7c545
João Victor Dias de Moraes <54956887+jvxdev@users.noreply.github.com>
2021-08-16 21:55:34 -0300

    Dashboard atualizada
    Tela de boas vindas sendo criada.

--- App.tsx
+++ App.tsx
@@ -28,5 +28,5 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <Dashboard />
     </ThemeProvider>
-    )
+    );
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 import { 
     Container,
-    Header
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
 } from './styles';
 
 export function Dashboard() {
     return (
     <Container>
         <Header>
-            <Text>Olá!</Text>
+            <UserWrapper>
+                <UserInfo>
+                    <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/54956887?v=4'}}
+                    />
+                    <User>
+                        <UserGreeting>Olá,</UserGreeting>
+                        <UserName>João</UserName>
+                    </User>
+                </UserInfo>
+            </UserWrapper>
         </Header>
     </Container>
     )

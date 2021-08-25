d69fb650868f89965eaac1649d42ad23d66ea4db
João Victor Dias de Moraes <54956887+jvxdev@users.noreply.github.com>
2021-08-25 14:05:58 -0300

    Estilização da interface de selecionar categoria
    Interface criada e estilizada.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'styled-components';
 
 import theme from './src/global/styles/theme';
 
-import { Register } from './src/screens/Register';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 import {
   useFonts,
@@ -26,7 +26,7 @@ export default function App() {
 
   return ( 
     <ThemeProvider theme={theme}>
-      <Register />
+      <CategorySelect />
     </ThemeProvider>
     );
 }

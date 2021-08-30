4b45bd40a20f3f504eb947c3bf6ad4ffe0953daa
João Victor Dias de Moraes <54956887+jvxdev@users.noreply.github.com>
2021-08-30 12:14:10 -0300

    Salvando novas transações
    Agora as transações são armazenadas.

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -4,7 +4,10 @@ import { Keyboard, Modal, TouchableWithoutFeedback, Alert } from 'react-native';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+
 import { useForm } from 'react-hook-form';
+import { useNavigation } from '@react-navigation/native';
 
 import { InputForm } from '../../components/Forms/InputForm';
 import { Button } from '../../components/Forms/Button';
@@ -28,13 +31,13 @@ interface FormData {
 
 const schema = Yup.object().shape({
     name: Yup
-    .string()
-    .required("O nome é obrigatório!"),
+        .string()
+        .required("O nome é obrigatório!"),
     amount: Yup
-    .number()
-    .typeError("Informe um valor númerico!")
-    .positive("O valor não pode ser negativo!")
-    .required("O preço é obrigatório!")
+        .number()
+        .typeError("Informe um valor númerico!")
+        .positive("O valor não pode ser negativo!")
+        .required("O preço é obrigatório!")
 });
 
 export function Register() {
@@ -48,10 +51,13 @@ export function Register() {
         name: 'Categoria'
     });
 
+    const navigation = useNavigation();
+
     const {
         control,
         handleSubmit,
-        formState: {errors}
+        reset,
+        formState: { errors }
     } = useForm({
         resolver: yupResolver(schema)
     });
@@ -77,16 +83,34 @@ export function Register() {
             return Alert.alert("Por favor, selecione a categoria.")
         }
 
-            const data = {
-                name: form.name,
-                amount: form.amount,
-                transactionType,
-                category: category.key
-            }
+        const newTransaction = {
+            id: String(uuid.v4()),
+            name: form.name,
+            amount: form.amount,
+            transactionType,
+            category: category.key,
+            date: new Date()
+        }
 
         try {
-            await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+            const data = await AsyncStorage.getItem(dataKey);
+            const currentData = data ? JSON.parse(data) : [];
 
+            const dataFormatted = [
+                ...currentData,
+                newTransaction
+            ];
+
+            await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+            reset();
+            setTransactionType('');
+            setCategory({
+                key: 'category',
+                name: 'Categoria'
+            });
+
+            navigation.navigate('LISTAGEM');
         } catch (error) {
             console.log(error);
             Alert.alert("Não foi possível salvar.")
@@ -96,7 +120,7 @@ export function Register() {
     useEffect(() => {
         async function loadData() {
             const data = await AsyncStorage.getItem(dataKey);
-            console.log(data);
+            console.log(JSON.parse(data!));
         }
 
         loadData();

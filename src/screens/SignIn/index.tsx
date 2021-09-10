7a844209cb3489b0494fc8a3c3455d764165b25c
João Victor Dias de Moraes <54956887+jvxdev@users.noreply.github.com>
2021-09-10 12:44:02 -0300

    Removido login com Apple no Android
    O Android não terá conta Apple como forma de login.

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ActivityIndicator, Alert } from 'react-native';
+import { ActivityIndicator, Alert, Platform } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import LogoSvg from '../../assets/logo.svg';
@@ -83,11 +83,14 @@ export function SignIn() {
                         onPress={handleSignInWithGoogle}
                     />
 
-                    <SignInSocialButton
-                        title="Entrar com Apple"
-                        svg={AppleSvg}
-                        onPress={handleSignInWithApple}
-                    />
+                    {
+                        Platform.OS === 'ios' &&
+                        <SignInSocialButton
+                            title="Entrar com Apple"
+                            svg={AppleSvg}
+                            onPress={handleSignInWithApple}
+                        />
+                    }
                 </FooterWrapper>
 
                 {isLoading &&
